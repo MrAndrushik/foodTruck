797ee40002f97a3bb2c049bf593d865835568bbc
Андрей Петров <mrandrushik@gmail.com>
2022-06-21 16:23:40 +0300

    new partia without bucket v-3

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,15 +1,15 @@
-// import Image from "next/image";
-// import Link from "next/link";
-// // next breadcrumb
-// import { Navigation } from "swiper";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import SlideBtn from "../../components/modules/SlideBtn";
+import Image from "next/image";
+import Link from "next/link";
+// next breadcrumb
+import { Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SlideBtn from "../../components/modules/SlideBtn";
 
-// import "swiper/css";
-// import "swiper/css/navigation";
+import "swiper/css";
+import "swiper/css/navigation";
 
-// import styles from "../../styles/modules/Events.module.scss";
-// import Breadcrumbs from "../../components/modules/Breadcrumbs";
+import styles from "../../styles/modules/Events.module.scss";
+import Breadcrumbs from "../../components/modules/Breadcrumbs";
 
 // export const getStaticPaths = async () => {
 //     const res = await fetch("http://localhost:3000/api/events");
@@ -36,60 +36,61 @@
 //     };
 // };
 
-// const EventsDetails = ({ events }) => {
-//     return (
-//         <section className={styles.section}>
-//             <Breadcrumbs />
-//             {events.map((event) => (
-//                 <div key={event.id} className={styles.container}>
-//                     <h1 className={styles.mainTitle}>{event.caption}</h1>
-//                     <Swiper
-//                         modules={[Navigation]}
-//                         slidesPerView="auto"
-//                         className={`${styles.swiper} gallery__swiper`}
-//                         grabCursor
-//                         autoHeight
-//                         breakpoints={{
-//                             1170: {
-//                                 spaceBetween: 20,
-//                                 centeredSlides: true,
-//                             },
-//                             0: {
-//                                 spaceBetween: 10,
-//                                 centeredSlides: true,
-//                             },
-//                         }}
-//                     >
-//                         {event.gallery.map((item, index) => (
-//                             <SwiperSlide
-//                                 className={styles.swiperSlide}
-//                                 key={index}
-//                             >
-//                                 <div className="slide-wrapper">
-//                                     <div className={`${styles.slide}`}>
-//                                         <Image
-//                                             src={item}
-//                                             layout="fill"
-//                                             alt="slide"
-//                                         />
-//                                     </div>
-//                                 </div>
-//                             </SwiperSlide>
-//                         ))}
-//                         <SlideBtn adaptive="desktop" type="next" />
-//                     </Swiper>
-//                     <div className={styles.textBlock}>
-//                         <h2 className={styles.descrTitle}>Описание</h2>
-//                         {event.fullDescrArr.map((descr, index) => (
-//                             <p key={index} className={styles.text}>
-//                                 {descr}
-//                             </p>
-//                         ))}
-//                     </div>
-//                 </div>
-//             ))}
-//         </section>
-//     );
-// };
+const EventsDetails = ({ events }) => {
+    // return (
+    //     <section className={styles.section}>
+    //         <Breadcrumbs />
+    //         {events.map((event) => (
+    //             <div key={event.id} className={styles.container}>
+    //                 <h1 className={styles.mainTitle}>{event.caption}</h1>
+    //                 <Swiper
+    //                     modules={[Navigation]}
+    //                     slidesPerView="auto"
+    //                     className={`${styles.swiper} gallery__swiper`}
+    //                     grabCursor
+    //                     autoHeight
+    //                     breakpoints={{
+    //                         1170: {
+    //                             spaceBetween: 20,
+    //                             centeredSlides: true,
+    //                         },
+    //                         0: {
+    //                             spaceBetween: 10,
+    //                             centeredSlides: true,
+    //                         },
+    //                     }}
+    //                 >
+    //                     {event.gallery.map((item, index) => (
+    //                         <SwiperSlide
+    //                             className={styles.swiperSlide}
+    //                             key={index}
+    //                         >
+    //                             <div className="slide-wrapper">
+    //                                 <div className={`${styles.slide}`}>
+    //                                     <Image
+    //                                         src={item}
+    //                                         layout="fill"
+    //                                         alt="slide"
+    //                                     />
+    //                                 </div>
+    //                             </div>
+    //                         </SwiperSlide>
+    //                     ))}
+    //                     <SlideBtn adaptive="desktop" type="next" />
+    //                 </Swiper>
+    //                 <div className={styles.textBlock}>
+    //                     <h2 className={styles.descrTitle}>Описание</h2>
+    //                     {event.fullDescrArr.map((descr, index) => (
+    //                         <p key={index} className={styles.text}>
+    //                             {descr}
+    //                         </p>
+    //                     ))}
+    //                 </div>
+    //             </div>
+    //         ))}
+    //     </section>
+    // );
+    return <div>SALO</div>;
+};
 
-// export default EventsDetails;
+export default EventsDetails;
--- pages/foodtrucks/[id].js
+++ pages/foodtrucks/[id].js
@@ -14,117 +14,118 @@ import CookData from "../../public/data/foodtrucks/details/cooking.json";
 import AdvantagesData from "../../public/data/foodtrucks/advantages.json";
 import RecipeData from "../../public/data/foodtrucks/details/recipe.json";
 
-export const getStaticPaths = async () => {
-    const res = await fetch("http://localhost:3000/api/foodtrucks");
-    const data = await res.json();
-    const paths = data.map((item) => {
-        return {
-            params: { id: item.id.toString() },
-        };
-    });
+// export const getStaticPaths = async () => {
+//     const res = await fetch("http://localhost:3000/api/foodtrucks");
+//     const data = await res.json();
+//     const paths = data.map((item) => {
+//         return {
+//             params: { id: item.id.toString() },
+//         };
+//     });
 
-    return {
-        paths,
-        fallback: false,
-    };
-};
+//     return {
+//         paths,
+//         fallback: false,
+//     };
+// };
 
-export const getStaticProps = async (context) => {
-    const id = context.params.id;
-    const res = await fetch("http://localhost:3000/api/foodtrucks/" + id);
-    const data = await res.json();
+// export const getStaticProps = async (context) => {
+//     const id = context.params.id;
+//     const res = await fetch("http://localhost:3000/api/foodtrucks/" + id);
+//     const data = await res.json();
 
-    return {
-        props: { foodtrucks: data },
-    };
-};
+//     return {
+//         props: { foodtrucks: data },
+//     };
+// };
 
 const FoodtruckDetails = ({ foodtrucks }) => {
-    const foodtruck = foodtrucks[0];
-    return (
-        <>
-            <section className={styles.section}>
-                <Breadcrumbs />
-                <h1 className={`${styles.title} stn-title`}>
-                    {foodtruck.caption}
-                </h1>
-                <Swiper
-                    modules={[Navigation]}
-                    slidesPerView="auto"
-                    className={`${styles.swiper} gallery__swiper`}
-                    grabCursor
-                    autoHeight
-                    breakpoints={{
-                        1170: {
-                            spaceBetween: 20,
-                            centeredSlides: true,
-                        },
-                        0: {
-                            spaceBetween: 10,
-                            centeredSlides: true,
-                        },
-                    }}
-                >
-                    {foodtruck.gallery.map((item, index) => (
-                        <SwiperSlide className={styles.swiperSlide} key={index}>
-                            <div className="slide-wrapper">
-                                <div className={`${styles.slide}`}>
-                                    <Image
-                                        src={item}
-                                        layout="fill"
-                                        alt="slide"
-                                    />
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                    <SlideBtn adaptive="desktop" type="next" />
-                </Swiper>
-            </section>
-            <section className={styles.characteristic}>
-                <h2 className={`${styles.caption} stn-title`}>
-                    {foodtruck.characteristic.title}
-                </h2>
-                <div className={styles.block}>
-                    {foodtruck.characteristic.information.map((item, index) => (
-                        <div key={index} className={styles.blockItem}>
-                            <p className={styles.blockCaption}>
-                                {item.caption}
-                            </p>
-                            <div className={styles.descrBlock}>
-                                {item.descr.map((descr, index) => (
-                                    <p key={index} className={styles.blockText}>
-                                        {descr}
-                                    </p>
-                                ))}
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </section>
-            <Advantages obj={CookData} border={false} background="right" />
-            <Advantages obj={AdvantagesData} />
-            <Advantages obj={RecipeData} border={false} background="right" />
-            <section className={styles.poster}>
-                <div className={styles.imgBlock}>
-                    <Image
-                        src="/img/foodtrucks/poster.png"
-                        layout="fill"
-                        alt="food"
-                        objectFit="cover"
-                    />
-                </div>
-                <div className={styles.content}>
-                    <p>
-                        В наличии более чем 30 видов блюд и более 15 видов
-                        оборудования для фудтрака.{" "}
-                        <span>Более подробно расскажет наш менеджер</span>
-                    </p>
-                    <button className={styles.btn}>Оставить заявку</button>
-                </div>
-            </section>
-        </>
-    );
+    // const foodtruck = foodtrucks[0];
+    // return (
+    //     <>
+    //         <section className={styles.section}>
+    //             <Breadcrumbs />
+    //             <h1 className={`${styles.title} stn-title`}>
+    //                 {foodtruck.caption}
+    //             </h1>
+    //             <Swiper
+    //                 modules={[Navigation]}
+    //                 slidesPerView="auto"
+    //                 className={`${styles.swiper} gallery__swiper`}
+    //                 grabCursor
+    //                 autoHeight
+    //                 breakpoints={{
+    //                     1170: {
+    //                         spaceBetween: 20,
+    //                         centeredSlides: true,
+    //                     },
+    //                     0: {
+    //                         spaceBetween: 10,
+    //                         centeredSlides: true,
+    //                     },
+    //                 }}
+    //             >
+    //                 {foodtruck.gallery.map((item, index) => (
+    //                     <SwiperSlide className={styles.swiperSlide} key={index}>
+    //                         <div className="slide-wrapper">
+    //                             <div className={`${styles.slide}`}>
+    //                                 <Image
+    //                                     src={item}
+    //                                     layout="fill"
+    //                                     alt="slide"
+    //                                 />
+    //                             </div>
+    //                         </div>
+    //                     </SwiperSlide>
+    //                 ))}
+    //                 <SlideBtn adaptive="desktop" type="next" />
+    //             </Swiper>
+    //         </section>
+    //         <section className={styles.characteristic}>
+    //             <h2 className={`${styles.caption} stn-title`}>
+    //                 {foodtruck.characteristic.title}
+    //             </h2>
+    //             <div className={styles.block}>
+    //                 {foodtruck.characteristic.information.map((item, index) => (
+    //                     <div key={index} className={styles.blockItem}>
+    //                         <p className={styles.blockCaption}>
+    //                             {item.caption}
+    //                         </p>
+    //                         <div className={styles.descrBlock}>
+    //                             {item.descr.map((descr, index) => (
+    //                                 <p key={index} className={styles.blockText}>
+    //                                     {descr}
+    //                                 </p>
+    //                             ))}
+    //                         </div>
+    //                     </div>
+    //                 ))}
+    //             </div>
+    //         </section>
+    //         <Advantages obj={CookData} border={false} background="right" />
+    //         <Advantages obj={AdvantagesData} />
+    //         <Advantages obj={RecipeData} border={false} background="right" />
+    //         <section className={styles.poster}>
+    //             <div className={styles.imgBlock}>
+    //                 <Image
+    //                     src="/img/foodtrucks/poster.png"
+    //                     layout="fill"
+    //                     alt="food"
+    //                     objectFit="cover"
+    //                 />
+    //             </div>
+    //             <div className={styles.content}>
+    //                 <p>
+    //                     В наличии более чем 30 видов блюд и более 15 видов
+    //                     оборудования для фудтрака.{" "}
+    //                     <span>Более подробно расскажет наш менеджер</span>
+    //                 </p>
+    //                 <button className={styles.btn}>Оставить заявку</button>
+    //             </div>
+    //         </section>
+    //     </>
+    // );
+    return <div>SALO</div>;
 };
 
 export default FoodtruckDetails;
