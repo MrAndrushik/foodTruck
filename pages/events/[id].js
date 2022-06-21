7071337dc431e78e2ccf91eb983cbd733a07be29
Андрей Петров <mrandrushik@gmail.com>
2022-06-21 16:19:45 +0300

    new partia without bucket

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,95 +1,95 @@
-import Image from "next/image";
-import Link from "next/link";
-// next breadcrumb
-import { Navigation } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SlideBtn from "../../components/modules/SlideBtn";
+// import Image from "next/image";
+// import Link from "next/link";
+// // next breadcrumb
+// import { Navigation } from "swiper";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import SlideBtn from "../../components/modules/SlideBtn";
 
-import "swiper/css";
-import "swiper/css/navigation";
+// import "swiper/css";
+// import "swiper/css/navigation";
 
-import styles from "../../styles/modules/Events.module.scss";
-import Breadcrumbs from "../../components/modules/Breadcrumbs";
+// import styles from "../../styles/modules/Events.module.scss";
+// import Breadcrumbs from "../../components/modules/Breadcrumbs";
 
-export const getStaticPaths = async () => {
-    const res = await fetch("http://localhost:3000/api/events");
-    const data = await res.json();
-    const paths = data.map((item) => {
-        return {
-            params: { id: item.id.toString() },
-        };
-    });
+// export const getStaticPaths = async () => {
+//     const res = await fetch("http://localhost:3000/api/events");
+//     const data = await res.json();
+//     const paths = data.map((item) => {
+//         return {
+//             params: { id: item.id.toString() },
+//         };
+//     });
 
-    return {
-        paths,
-        fallback: false,
-    };
-};
+//     return {
+//         paths,
+//         fallback: false,
+//     };
+// };
 
-export const getStaticProps = async (context) => {
-    const id = context.params.id;
-    const res = await fetch("http://localhost:3000/api/events/" + id);
-    const data = await res.json();
+// export const getStaticProps = async (context) => {
+//     const id = context.params.id;
+//     const res = await fetch("http://localhost:3000/api/events/" + id);
+//     const data = await res.json();
 
-    return {
-        props: { events: data },
-    };
-};
+//     return {
+//         props: { events: data },
+//     };
+// };
 
-const EventsDetails = ({ events }) => {
-    return (
-        <section className={styles.section}>
-            <Breadcrumbs />
-            {events.map((event) => (
-                <div key={event.id} className={styles.container}>
-                    <h1 className={styles.mainTitle}>{event.caption}</h1>
-                    <Swiper
-                        modules={[Navigation]}
-                        slidesPerView="auto"
-                        className={`${styles.swiper} gallery__swiper`}
-                        grabCursor
-                        autoHeight
-                        breakpoints={{
-                            1170: {
-                                spaceBetween: 20,
-                                centeredSlides: true,
-                            },
-                            0: {
-                                spaceBetween: 10,
-                                centeredSlides: true,
-                            },
-                        }}
-                    >
-                        {event.gallery.map((item, index) => (
-                            <SwiperSlide
-                                className={styles.swiperSlide}
-                                key={index}
-                            >
-                                <div className="slide-wrapper">
-                                    <div className={`${styles.slide}`}>
-                                        <Image
-                                            src={item}
-                                            layout="fill"
-                                            alt="slide"
-                                        />
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                        ))}
-                        <SlideBtn adaptive="desktop" type="next" />
-                    </Swiper>
-                    <div className={styles.textBlock}>
-                        <h2 className={styles.descrTitle}>Описание</h2>
-                        {event.fullDescrArr.map((descr, index) => (
-                            <p key={index} className={styles.text}>
-                                {descr}
-                            </p>
-                        ))}
-                    </div>
-                </div>
-            ))}
-        </section>
-    );
-};
+// const EventsDetails = ({ events }) => {
+//     return (
+//         <section className={styles.section}>
+//             <Breadcrumbs />
+//             {events.map((event) => (
+//                 <div key={event.id} className={styles.container}>
+//                     <h1 className={styles.mainTitle}>{event.caption}</h1>
+//                     <Swiper
+//                         modules={[Navigation]}
+//                         slidesPerView="auto"
+//                         className={`${styles.swiper} gallery__swiper`}
+//                         grabCursor
+//                         autoHeight
+//                         breakpoints={{
+//                             1170: {
+//                                 spaceBetween: 20,
+//                                 centeredSlides: true,
+//                             },
+//                             0: {
+//                                 spaceBetween: 10,
+//                                 centeredSlides: true,
+//                             },
+//                         }}
+//                     >
+//                         {event.gallery.map((item, index) => (
+//                             <SwiperSlide
+//                                 className={styles.swiperSlide}
+//                                 key={index}
+//                             >
+//                                 <div className="slide-wrapper">
+//                                     <div className={`${styles.slide}`}>
+//                                         <Image
+//                                             src={item}
+//                                             layout="fill"
+//                                             alt="slide"
+//                                         />
+//                                     </div>
+//                                 </div>
+//                             </SwiperSlide>
+//                         ))}
+//                         <SlideBtn adaptive="desktop" type="next" />
+//                     </Swiper>
+//                     <div className={styles.textBlock}>
+//                         <h2 className={styles.descrTitle}>Описание</h2>
+//                         {event.fullDescrArr.map((descr, index) => (
+//                             <p key={index} className={styles.text}>
+//                                 {descr}
+//                             </p>
+//                         ))}
+//                     </div>
+//                 </div>
+//             ))}
+//         </section>
+//     );
+// };
 
-export default EventsDetails;
+// export default EventsDetails;
